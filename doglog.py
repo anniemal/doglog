295a916b14226b49bd5eab421b4a7df79aabf051
Louise Fox <louise.s.fox@gmail.com>
2012-11-27 15:02:03 -0800

    updated Twilio SMS messaging

--- doglog.py
+++ doglog.py
@@ -150,8 +150,9 @@ def m_save_map():
     account = "AC7225c1d30d2cce103ea56289e3fc6ed8"
     token = "6efbc4e502a9672e69fddf93c981cbbe"
     client = TwilioRestClient(account, token)
-
-    message = client.sms.messages.create(to="+15625474270", from_="+14155994769", body="%s walked for %g miles for %s time.") % (dog.dog_name, elapsed_distance, elapsed_time)
+    message_str="%s walked for %g miles for %s time." % (dog.dog_name, elapsed_distance, elapsed_time)
+    print message_str
+    message = client.sms.messages.create(to="+15625474270", from_="+14155994769", body=message_str) 
 
 
 
